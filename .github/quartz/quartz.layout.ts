2a80ad1450b805c80605a1c9c9c308e47a718417
Lilian Miller <117693278+Lynxee123@users.noreply.github.com>
2025-08-02 13:44:04 -0700

    Update quartz.layout.ts

--- .github/quartz/quartz.layout.ts
+++ .github/quartz/quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      "GitHub": "https://github.com/growlf/journal",
+      "GitHub": "https://github.com/lynxee123/journal",
     },
   }),
 }
